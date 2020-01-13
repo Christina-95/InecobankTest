67c3c72c53b55e2dc9d04025c9df39128826aec2
Christina-95 <christinasargsyan@gmail.com>
2020-01-14 01:48:47 +0400

    property file added

--- src/main/java/am/inecobank/pages/home/header/InecoBankHeaderLinks.java
+++ src/main/java/am/inecobank/pages/home/header/InecoBankHeaderLinks.java
@@ -19,6 +19,7 @@ public class InecoBankHeaderLinks extends InecoBankHomePage {
 	public static final String AYL = "(//span[@class='secondaryNav__action secondaryNav__action--noAction'])[5]";
 	public static final String TVAYIN_BANK = "(//span[@class='secondaryNav__action secondaryNav__action--noAction'])[4]";
 	public static final String CARDS = "//a[@class='secondaryNav__action']";
+	public static final String HASHIVNER = "(//span[@class='secondaryNav__action secondaryNav__action--noAction'])[1]";
 	
 	@FindBy (xpath = AYL)
 	WebElement aylLink;
@@ -28,6 +29,9 @@ public class InecoBankHeaderLinks extends InecoBankHomePage {
 	
 	@FindBy (xpath = CARDS)
 	WebElement cardsLink;
+	
+	@FindBy (xpath = HASHIVNER)
+	WebElement hashivnerLink;
 	
 	public void waitForAyl() {
 		wait.waitForElementVisibility(aylLink);
@@ -51,5 +55,13 @@ public class InecoBankHeaderLinks extends InecoBankHomePage {
 	
 	public void moveToCards() {
 		actions.moveToElement(cardsLink);
+	}
+	
+	public void waitForHashivner() {
+		wait.waitForElementVisibility(hashivnerLink);
+	}
+	
+	public void moveToHashivner() {
+		actions.moveToElement(hashivnerLink);
 	}
 }
--- src/test/java/am/inecobank/base/BaseTest.java
+++ src/test/java/am/inecobank/base/BaseTest.java
@@ -1,21 +1,24 @@
 package am.inecobank.base;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
-	
-	protected String inecoBankUrl = "https://www.inecobank.am/hy/Individual";
-	
+	private Properties properties;
 	protected WebDriver driver;
 	
 	@BeforeTest
-	public void beginning() {
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+	public void beginning() throws IOException {
+		properties = new Properties();
+		properties.load(getClass().getClassLoader().getResourceAsStream("readProperty.properties"));
+		System.setProperty("webdriver.chrome.driver", properties.getProperty("chromeDriver"));
 		driver = new ChromeDriver();
-		driver.get(inecoBankUrl);	
+		driver.get(properties.getProperty("inecoBankUrl"));	
 		//driver.manage().window().maximize();
 	}
 	
